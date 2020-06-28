f800bc8398ba9251f1704b2a5bda2e78447fe9e2
lucas <lucas.laborne@gmail.com>
2020-06-28 10:30:39 -0300

    world_time

---
+++ world_time/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    );
+  }
+}
---
+++ world_time/lib/pages/loading.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/services/worldtime.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> with SingleTickerProviderStateMixin {
+  String time = 'loading';
+  Animation _animation;
+  AnimationController _controller;
+
+  void setupWorldTime() async {
+    WorldTimeService wt = WorldTimeService(
+      location: 'America',
+      flag: 'brazil.png',
+      url: 'America/Sao_Paulo',
+    );
+    await wt.getTime();
+    // Delay proposital para mostrar a animação:
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': wt.location,
+      'flag': wt.flag,
+      'time': wt.time,
+    });
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: SpinKitCubeGrid(
+              color: Colors.white,
+              size: 100,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
